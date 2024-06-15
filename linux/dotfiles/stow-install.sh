2cc1b7055d0990c3567e43a4348016a237dd411b
Cragady <pinkdotdreamer@gmail.com>
2024-06-15 17:51:24 -0600

    feat: structure dotfiles for stow install control

---
+++ linux/dotfiles/stow-install.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+STOW_FOLDERS="conky,editorconfig,shells,tmux,vim"
+
+for folder in $(echo $STOW_FOLDERS | sed "s/,/ /g")
+do
+  echo "stow $folder"
+  stow $folder -t $HOME
+  if (( $? == 1 )); then
+    echo "stow failed, attempting to adopt $folder"
+    stow --adopt $folder -t $HOME
+  fi
+done
