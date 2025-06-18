debba4a9da7000867ef4db0591ce9871c015a246
Cragady <pinkdotdreamer@gmail.com>
2025-06-17 20:00:53 -0600

    chore: update config for peek.nvim
    * chore: update plugins

--- dotfiles/nvim/.config/nvim/lua/plugins/markdown.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/markdown.lua
@@ -20,11 +20,35 @@ return {
     event = { "VeryLazy" },
     -- WARN: requires deno
     build = "deno task --quiet build:fast",
-    config = function()
-      -- require("peek").setup({
-      --   filetype = { 'markdown', 'conf' }
-      -- })
-      require('peek').setup()
+    opts = {
+      auto_load = true,         -- whether to automatically load preview when
+                                -- entering another markdown buffer
+      close_on_bdelete = true,  -- close preview window on buffer delete
+
+      syntax = true,            -- enable syntax highlighting, affects performance
+
+      theme = 'dark',           -- 'dark' or 'light'
+
+      update_on_change = true,
+
+      -- app = 'webview',
+      -- app = 'browser',
+      -- app = 'browser',
+      -- app = { 'chromium', '--new-window' },
+      app = 'webview',          -- 'webview', 'browser', string or a table of strings
+                                -- explained below
+
+      -- filetype = { 'markdown', 'conf' },
+      filetype = { 'markdown' },-- list of filetypes to recognize as markdown
+
+      -- relevant if update_on_change is true
+      throttle_at = 200000,     -- start throttling when file exceeds this
+                                -- amount of bytes in size
+      throttle_time = 'auto',   -- minimum amount of time in milliseconds
+                                -- that has to pass before starting new render
+    },
+    config = function(_, opts)
+      require('peek').setup(opts)
       vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
       vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
     end,
