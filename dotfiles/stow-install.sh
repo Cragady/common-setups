bfad2a23e25ab12d67a656e9f92152f9b51e243c
Cragady <craigwright2048@gmail.com>
2026-06-01 18:05:26 -0600

    chore: add note on stow-install

--- dotfiles/stow-install.sh
+++ dotfiles/stow-install.sh
@@ -1,5 +1,9 @@
 #!/bin/zsh
 
+# NOTE: may need to implement a refresh of sorts of what binaries/executables
+# are on the path. This is useful in the case of removed items no longer being
+# owned by stow, and not available on path anymore.
+
 . $HOME/common-setups/dotfiles/stow-config.sh
 
 for folder in $(echo $STOW_FOLDERS | sed "s/,/ /g")
